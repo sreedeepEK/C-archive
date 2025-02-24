0577c23c1cf3785529e5de027a250095e77b6e1f
Sreedeep . <sreedeepek.95@gmail.com>
2025-02-24 11:26:32 +0530

    add struct.c

---
+++ struct.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+struct Player
+{
+
+    char name[12];
+    int score;
+
+};
+
+int main()
+{
+    // struct= collection of relateed member (variable)
+    // they can be of different data type
+    // listed under one name in a block of memory (like class in python)
+
+    struct Player player1;
+    struct Player player2;
+
+    strcpy(player1.name, "Bro");
+    player1.score = 4;
+
+
+    strcpy(player2.name, "Bro 2");
+    player1.score = 3;
+
+    printf("%s\n", player1.name);
+    printf("%d\n",player1.score);
+
+    return 0;
+}
